8d2be7be4a2ccf5e13028dd590eeae698152e935
wisehackermonkey <oranbusiness@gmail.com>
2019-09-24 10:29:35 -0700

    dynamic version number, feature flags

--- bin/ardinst.js
+++ bin/ardinst.js
@@ -11,6 +11,8 @@
  20190224
 */
 const program = require("commander");
+const config = require("../config.json");
+const package_json = require("../package.json");
 
 // TODO change font
 console.log(`
@@ -24,13 +26,25 @@ console.log(`
 /////////////////////////////////`);
 console.log(`Ardinst (arduino install) the arduino package manager`);
 
-program
-    .version('1.2.1', '-v, --version')
+if(config.showUploadTools){
+  program
+    .version(package_json.version, '-v, --version')
     .command('install [library]', 'install one or more packages')
     .command('uninstall [library]', 'remove packages by specifing package name, allows for multiple uninstalls at same time')
     .command('search [query]', 'search npm for arduino-* packages')
-    .command('generate [folder]', 'generate arduino library file project template')
-    .command('update [library]', 'installs newest version of libraries')
     .command('list', 'lists the packages installed')
+    .command('update [library]', 'installs newest version of libraries')
+
+    .command('generate [folder]', 'generate arduino library file project template')
     .command('publish',"upload's arduino library to npm, with all necessary files")
     .parse(process.argv);
+}else{
+  program
+    .version(package_json.version, '-v, --version')
+    .command('install [library]', 'install one or more packages')
+    .command('uninstall [library]', 'remove packages by specifing package name, allows for multiple uninstalls at same time')
+    .command('search [query]', 'search npm for arduino-* packages')
+    .command('list', 'lists the packages installed')
+    .command('update [library]', 'installs newest version of libraries')
+    .parse(process.argv);
+}
